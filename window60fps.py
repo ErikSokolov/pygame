132c40e27a89d7b3463d0df692a139e20cfc210a
Erik <erikstankevich@yandex.com>
2024-11-18 07:19:23 +0100

    Add and finish window.py and window60fps.py

---
+++ window60fps.py
@@ -0,0 +1,24 @@
+import pygame, sys
+
+clock = pygame.time.Clock()
+
+from pygame.locals import *
+
+pygame.init()
+
+pygame.display.set_caption('60fps Window')
+
+WINDOW_SIZE = (400, 400)
+
+screen = pygame.display.set_mode(WINDOW_SIZE,0,32)
+
+while True:
+    
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(60)
+
